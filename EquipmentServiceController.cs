f63628105cb957c0e67181a3a51676af232fdcdf
hienvvhutech <hienvvhien@gmail.com>
2025-03-12 23:06:01 +0700

    Add files via upload

---
+++ EquipmentServiceController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EquipmentService.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentService.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EquipmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Equipments.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(equipment);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+            return View(equipment);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Equipment equipment)
+        {
+            if (id != equipment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentExists(equipment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(equipment);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipment = await _context.Equipments.FindAsync(id);
+            _context.Equipments.Remove(equipment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentExists(int id)
+        {
+            return _context.Equipments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EquipmentService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
